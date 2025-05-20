9e92d2eeeb53ae3082b1e4ec7f6f3fb09717d433
supperwu <supperwu4781@hotmail.com>
2025-05-20 20:34:59 +0800

    Add Color Controlled Component

--- src/colororganizer/AddColorForm.jsx
+++ src/colororganizer/AddColorForm.jsx
@@ -1,3 +1,4 @@
+/* useRef
 import React, { useRef } from "react";
 
 export default function AddColorForm({ onNewColor = f => f }) {
@@ -17,6 +18,40 @@ export default function AddColorForm({ onNewColor = f => f }) {
     <form onSubmit={submit}>
       <input ref={txtTitle} type="text" placeholder="color title..." required />
       <input ref={hexColor} type="color" required />
+      <button>ADD</button>
+    </form>
+  );
+}
+  */
+
+import React, { useState } from "react";
+
+export default function AddColorForm({ onNewColor = f => f }) {
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
+
+  const submit = e => {
+    e.preventDefault();
+    onNewColor(title, color);
+    setTitle("");
+    setColor("");
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={color}
+        onChange={event => setColor(event.target.value)}
+        type="color"
+        required
+      />
       <button>ADD</button>
     </form>
   );
